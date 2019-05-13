aff14f893a2ab4ab28df7833b90498d60050d358
Adam Stein <adam@csh.rit.edu>
2019-05-13 14:55:50 -0400

    Updated version number to reflect difference after merging changes in base master

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='django-behave',
     packages=['django_behave'],
-    version='0.1.6',
+    version='0.1.6.1',
     description='Django Test Runner for the Behave BDD module',
     author='Rachel Willmer',
     author_email='rachel@willmer.org',
